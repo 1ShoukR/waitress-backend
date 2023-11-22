e91368cc9fdfd0c4cb97412228110623d32d1742
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-11-21 23:06:53 -0500

    created models associated with a staff

--- webapp/models/staff.py
+++ webapp/models/staff.py
@@ -2,15 +2,18 @@ import sqlalchemy as sa
 import typing as t
 from . import db
 from sqlalchemy import and_, cast, or_, orm
+from .user import Person
 if t.TYPE_CHECKING:
     from datetime import datetime
 
 
 
 
-class Staff(db.Model):
-    id = sa.Column(sa.Integer, primary_key=True)
-    name = sa.Column(sa.String(100), nullable=False)
+class Staff(Person):
+    __tablename__ = 'staff'
+    __mapper_args__ = {'polymorphic_identity': 'staff'}
+
+    staff_id = sa.Column(sa.Integer, sa.ForeignKey('person.person_id'), primary_key=True)
     role = sa.Column(sa.String(50), nullable=False)
     restaurant_id = sa.Column(sa.Integer, sa.ForeignKey('restaurant.restaurant_id'), nullable=False)
     # Additional fields as required
