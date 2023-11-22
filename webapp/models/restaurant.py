17aab16dcfd5e1c8fc6c84b7f50a43f003c6b336
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-11-21 23:05:42 -0500

    created models associated with a restaurant

--- webapp/models/restaurant.py
+++ webapp/models/restaurant.py
@@ -7,6 +7,16 @@ if t.TYPE_CHECKING:
 
 
 
+class Receipt(db.Model):
+    receipt_id = sa.Column(sa.Integer, primary_key=True)
+    tip_amount = sa.Column(sa.Float, nullable=True)
+    assigned_waiter = sa.Column(sa.Integer, sa.ForeignKey('staff.staff_id'))
+    restaurant_id = sa.Column(sa.Integer, sa.ForeignKey('restaurant.restaurant_id'))  # Foreign key to Restaurant
+
+    # Explicit relationship to Restaurant
+    restaurant = orm.relationship('Restaurant', foreign_keys=[restaurant_id])
+
+
 class Restaurant(db.Model):
     __tablename__ = 'restaurant'
     __table_args__ = {'extend_existing': True}
@@ -19,4 +29,35 @@ class Restaurant(db.Model):
     email:str = sa.Column(sa.String(255), nullable=False)
     website:str = sa.Column(sa.String(255))
     
-    
+    # Explicit relationship to Receipt
+    receipts = orm.relationship('Receipt', foreign_keys='Receipt.restaurant_id')
+
+class Reservation(db.Model): 
+    __tablename__ = 'reservation'
+    __table_args__ = {'extend_existing': True}
+    reservation_id = sa.Column(sa.Integer, primary_key=True)
+    user_id:int = sa.Column(sa.Integer, sa.ForeignKey('user.user_id'))
+    table_id:int = sa.Column(sa.Integer, sa.ForeignKey('table.table_id'))
+    time = sa.Column(sa.DateTime, nullable=False)
+
+    restaurant = orm.relationship('Restaurant', foreign_keys='Restaurant.restaurant_id')
+    customer = orm.relationship('User', foreign_keys='User.user_id')
+
+
+class MenuItem(db.Model):
+    __tablename__ = 'menu_item'
+    __table_args__ = {'extend_existing': True}
+    menu_id:int = sa.Column(sa.Integer, primary_key=True)
+    restaurant_id = sa.Column(sa.Integer, sa.ForeignKey('restaurant.restaurant_id'), nullable=False)
+    name_of_item = sa.Column(sa.String(200))
+    price = sa.Column(sa.Float)
+    is_available = sa.Column(sa.Boolean, default=True)
+
+
+    restaurant = orm.relationship('Restaurant', foreign_keys='Restaurant.restaurant_id')
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    __table_args__ = {'extend_existing': True}
+    order_id:int = sa.Column(sa.Integer, primary_key=True)
+    reservation_id:int = sa.Column(sa.Integer, sa.ForeignKey('reservation.reservation_id'))
