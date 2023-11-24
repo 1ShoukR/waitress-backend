df2d12306c1c235dc68b67fd32b8f16b2fd61f91
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-11-23 20:45:31 -0500

    adjusted staff models to map to inherit User traits

--- webapp/models/staff.py
+++ webapp/models/staff.py
@@ -2,18 +2,21 @@ import sqlalchemy as sa
 import typing as t
 from . import db
 from sqlalchemy import and_, cast, or_, orm
-from .user import Person
+from .user import User
 if t.TYPE_CHECKING:
     from datetime import datetime
 
 
 
 
-class Staff(Person):
+class Staff(User):
+    """
+    Represents a singular Staff user in thge database
+    """
     __tablename__ = 'staff'
     __mapper_args__ = {'polymorphic_identity': 'staff'}
 
-    staff_id = sa.Column(sa.Integer, sa.ForeignKey('person.person_id'), primary_key=True)
+    staff_id = sa.Column(sa.Integer, sa.ForeignKey('user.user_id'), primary_key=True)
     role = sa.Column(sa.String(50), nullable=False)
     restaurant_id = sa.Column(sa.Integer, sa.ForeignKey('restaurant.restaurant_id'), nullable=False)
     # Additional fields as required
