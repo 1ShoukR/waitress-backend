cf38140eb6a10520c9e38cee5f57175db4a177e0
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-11-23 20:46:07 -0500

    adjusted receipt model to have restaruant and receipt owner relationships

--- webapp/models/restaurant.py
+++ webapp/models/restaurant.py
@@ -11,10 +11,15 @@ class Receipt(db.Model):
     receipt_id = sa.Column(sa.Integer, primary_key=True)
     tip_amount = sa.Column(sa.Float, nullable=True)
     assigned_waiter = sa.Column(sa.Integer, sa.ForeignKey('staff.staff_id'))
+    assigned_user = sa.Column(sa.Integer, sa.ForeignKey('user.user_id'))
     restaurant_id = sa.Column(sa.Integer, sa.ForeignKey('restaurant.restaurant_id'))  # Foreign key to Restaurant
+    created_at:'datetime' = sa.Column(sa.Date)
+    updated_at:'datetime' = sa.Column(sa.Date)
+    deleted_at:'datetime' = sa.Column(sa.DateTime, nullable=True)
 
     # Explicit relationship to Restaurant
     restaurant = orm.relationship('Restaurant', foreign_keys=[restaurant_id])
+    receipt_owner = orm.relationship('User', foreign_keys=[assigned_user])
 
 
 class Restaurant(db.Model):
@@ -28,6 +33,10 @@ class Restaurant(db.Model):
     phone:str = sa.Column(sa.String(255), nullable=False)
     email:str = sa.Column(sa.String(255), nullable=False)
     website:str = sa.Column(sa.String(255))
+    number_of_tables:int = sa.Column(sa.Integer)
+    created_at:'datetime' = sa.Column(sa.Date)
+    updated_at:'datetime' = sa.Column(sa.Date)
+    deleted_at:'datetime' = sa.Column(sa.DateTime, nullable=True)
     
     # Explicit relationship to Receipt
     receipts = orm.relationship('Receipt', foreign_keys='Receipt.restaurant_id')
@@ -39,6 +48,10 @@ class Reservation(db.Model):
     user_id:int = sa.Column(sa.Integer, sa.ForeignKey('user.user_id'))
     table_id:int = sa.Column(sa.Integer, sa.ForeignKey('table.table_id'))
     time = sa.Column(sa.DateTime, nullable=False)
+    reservation_phone_number = sa.Column(sa.String(200))
+    created_at:'datetime' = sa.Column(sa.Date)
+    updated_at:'datetime' = sa.Column(sa.Date)
+    deleted_at:'datetime' = sa.Column(sa.DateTime, nullable=True)
 
     restaurant = orm.relationship('Restaurant', foreign_keys='Restaurant.restaurant_id')
     customer = orm.relationship('User', foreign_keys='User.user_id')
@@ -52,6 +65,9 @@ class MenuItem(db.Model):
     name_of_item = sa.Column(sa.String(200))
     price = sa.Column(sa.Float)
     is_available = sa.Column(sa.Boolean, default=True)
+    created_at:'datetime' = sa.Column(sa.Date)
+    updated_at:'datetime' = sa.Column(sa.Date)
+    deleted_at:'datetime' = sa.Column(sa.DateTime, nullable=True)
 
 
     restaurant = orm.relationship('Restaurant', foreign_keys='Restaurant.restaurant_id')
@@ -61,3 +77,10 @@ class Order(db.Model):
     __table_args__ = {'extend_existing': True}
     order_id:int = sa.Column(sa.Integer, primary_key=True)
     reservation_id:int = sa.Column(sa.Integer, sa.ForeignKey('reservation.reservation_id'))
+    user_id:int = sa.Column(sa.Integer, sa.ForeignKey('user.user_id'))
+    total:float = sa.Column(sa.Float)
+    is_paid:bool = sa.Column(sa.Boolean, default=False)
+
+    restaurant = orm.relationship('Restaurant', foreign_keys='Restaurant.restaurant_id')
+    reservation = orm.relationship('Reservation', foreign_keys='Reservation.reservation_id')
+    customer = orm.relationship('User', foreign_keys='User.user_id')
