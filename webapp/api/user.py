8287261916d17369c5eb1b5ad6827894c15fc1c3
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-11-23 20:43:16 -0500

    created basic user create route

--- webapp/api/user.py
+++ webapp/api/user.py
@@ -11,9 +11,23 @@ from passlib.hash import sha256_crypt
 from .. import models
 bp = Blueprint('user', __name__)
 
-@bp.route('/create')
+
+
+
+@bp.route('/create', methods=["POST"])
 def create():
     """
     Creates a new user. 
     """
+    if request.json.get('waiter'):
+        new_waiter = models.User(
+            first_name=request.json.get('first_name'),
+            last_name=request.json.get('last_name'),
+            email=request.json.get('email_name'),
+            # password_hash=request.json.get('first_name'),
+            type='waiter'  # Set the type for polymorphic identity
+        )
+        models.db.session.add(new_waiter)
+
+        return 
     return 'SUCCESS'
