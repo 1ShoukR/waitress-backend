c2c50cf9875e6a29b2d4c3d23d008e8c5d2c8b1d
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-11-23 20:46:51 -0500

    added new columns to table model as well as new relationships to customer and a reservation

--- webapp/models/table.py
+++ webapp/models/table.py
@@ -13,7 +13,17 @@ class Table(db.Model):
     Represents a single table in a restaurant and the number of people it can fit
     """
     table_id = sa.Column(sa.Integer, primary_key=True)
-    restaurant_id = sa.Column(sa.Integer, db.ForeignKey('restaurant.restaurant_id'), nullable=False)
+    restaurant_id = sa.Column(sa.Integer, sa.ForeignKey('restaurant.restaurant_id'), nullable=False)
+    reservation_id = sa.Column(sa.Integer, sa.ForeignKey('reservation.reservation_id'))
     table_number = sa.Column(sa.Integer, nullable=False)
     capacity = sa.Column(sa.Integer, nullable=False)
     location_description = sa.Column(sa.String(200))  # Description of the table's location
+    is_reserved:bool = sa.Column(sa.Boolean, default=False)
+    customer_id:int = sa.Column(sa.Integer, sa.ForeignKey('user.user_id'))
+    created_at:'datetime' = sa.Column(sa.Date)
+    updated_at:'datetime' = sa.Column(sa.Date)
+    deleted_at:'datetime' = sa.Column(sa.DateTime, nullable=True)
+
+
+    customer = orm.relationship('User', foreign_keys='User.user_id')
+    reservation = orm.relationship('Reservation', foreign_keys='Reservation.reservation_id')
