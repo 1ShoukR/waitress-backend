830e389e9cedb6e9d706aaa54ddd8315f6ab9b26
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-11-23 19:56:13 -0500

    created two api routes for creating restaurant and reserving a table

---
+++ webapp/api/restaurant.py
@@ -0,0 +1,24 @@
+from flask import (
+    Blueprint, 
+    current_app,
+    jsonify,
+    redirect,
+    request,
+    session,
+    url_for,
+)
+from passlib.hash import sha256_crypt
+from .. import models
+bp = Blueprint('restaurant', __name__)
+
+
+
+@bp.route('/create', methods=['POST'])
+def create():
+    return "I am working"
+
+
+
+@bp.route('/tables/<int:id>/reserve', methods=["POST"])
+def reserve(table_id):
+    pass
