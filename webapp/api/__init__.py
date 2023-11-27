91ed2bdb47a316bb39f4af4dafa1c40dd2eb1d43
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-11-26 20:39:19 -0500

    Made init file for the api folder itself

--- webapp/api/__init__.py
+++ webapp/api/__init__.py
@@ -1,6 +1,11 @@
-from . import (
-    auth,
-    restaurant,
-    setup,
-    user
-)
+from . import errorhandeling
+#: Convenience import alias for errorhandling.ERRORS
+ERRORS = errorhandeling.ERRORS
+#: Convenience import alias for errorhandling.APIError
+APIError = errorhandeling.APIError
+#: Convenience import alias for errorhandling.HTTPError
+HTTPError = errorhandeling.HTTPError
+#: Convenience import alias for errorhandling.ErrorCode
+ErrorCode = errorhandeling.ErrorCode
+
+from . import routes
