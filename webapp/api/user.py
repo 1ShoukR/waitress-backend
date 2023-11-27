0df20c5726a7b1b86461836400e1ddb6d94c0baf
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-11-26 19:14:43 -0500

    added a basic route to handle a waiter and a customer account creation

--- webapp/api/user.py
+++ webapp/api/user.py
@@ -16,20 +16,23 @@ bp = Blueprint('user', __name__)
 
 @bp.route('/create', methods=["POST"])
 def create():
+    # FUTUREUS Add a Waiter to a Staff object
     """
     Creates a new user. 
     """
     print(request.json)
-    waiter_data = request.json.get('waiter')
-    if waiter_data:
-        new_waiter = models.User(
-            first_name=waiter_data['first_name'],
-            last_name=waiter_data['last_name'],
-            email=waiter_data['email'],
-            password_hash=waiter_data['first_name'],  
-            type='waiter'  
+    user_data = request.json.get('waiter') or request.json.get('customer')
+    if user_data:
+        hash = sha256_crypt.using(rounds=1000).hash(user_data['password'])
+        user_type = 'waiter' if request.json.get('waiter') else 'customer'
+        new_user = models.User(
+            first_name=user_data['first_name'],
+            last_name=user_data['last_name'],
+            email=user_data['email'],
+            password_hash=hash,  
+            type=user_type 
         )
-        models.db.session.add(new_waiter)
+        models.db.session.add(new_user)
         models.db.session.commit()
         return jsonify({'success': True})
     return 'SUCCESS'
