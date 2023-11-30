46e8928f7db5e22c8e6602f3426c81576b3554fe
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-11-30 15:07:26 -0500

    added basic auth logic for future testing

--- webapp/api/routes/auth.py
+++ webapp/api/routes/auth.py
@@ -7,6 +7,9 @@ from flask import (
     session,
     url_for,
 )
+import sqlalchemy as sa
+from ...api import ERRORS
+from ... import models
 
 bp = Blueprint('auth', __name__)
 
@@ -14,4 +17,8 @@ bp = Blueprint('auth', __name__)
 
 @bp.route('/login', methods=['POST'])
 def login():
+    print('erros', ERRORS)
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = models.User.query.filter(sa.func.lower(models.User.email) == email.lower())
     return 'hit'
