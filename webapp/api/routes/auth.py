13c43c034a34c1639bda5209d7cfc7e013fca044
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-04 09:45:32 -0500

    adjusted login route

--- webapp/api/routes/auth.py
+++ webapp/api/routes/auth.py
@@ -17,8 +17,13 @@ bp = Blueprint('auth', __name__)
 
 @bp.route('/login', methods=['POST'])
 def login():
-    print('erros', ERRORS)
     email = request.form.get('email')
     password = request.form.get('password')
     user = models.User.query.filter(sa.func.lower(models.User.email) == email.lower())
+
+    if request.json['user_agent']:
+        #  Right here we are gonna differentiate what client is logged in
+        #  we will the query the client based on what the user agent is 
+        return 'user agent'
+
     return 'hit'
