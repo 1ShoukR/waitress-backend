859d96522ca9a4c5e56b98589125597dba96e5f2
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-05 23:46:40 -0500

    added logic to find a user based on web client or not

--- webapp/api/routes/auth.py
+++ webapp/api/routes/auth.py
@@ -8,8 +8,10 @@ from flask import (
     url_for,
 )
 import sqlalchemy as sa
-from ...api import ERRORS
+from ...utils.auth import create_api_token
+from ...api import ERRORS, jsonify_error_code
 from ... import models
+from passlib.hash import sha256_crypt
 
 bp = Blueprint('auth', __name__)
 
@@ -19,11 +21,14 @@ bp = Blueprint('auth', __name__)
 def login():
     email = request.form.get('email')
     password = request.form.get('password')
-    user = models.User.query.filter(sa.func.lower(models.User.email) == email.lower())
-
-    if request.json['user_agent']:
-        #  Right here we are gonna differentiate what client is logged in
-        #  we will the query the client based on what the user agent is 
-        return 'user agent'
-
-    return 'hit'
+    user = models.User.query.filter(sa.func.lower(models.User.email) == email.lower()).first_or_404()
+    if user:
+        verify_password = sha256_crypt.verify(hash=user.password_hash, secret=password)
+        if verify_password:
+            if 'web' in request.form.get('user_agent'):
+                client = models.APIClient.query.filter_by(public_uid='web').first_or_404()
+            else:
+                client = models.APIClient.query.filter_by(public_uid='mobile').first_or_404()
+            token = create_api_token(client_id=client.client_id, user_id=user.user_id)
+            return jsonify(user=user.first_name, user_type=user.type, token=token)
+    return jsonify_error_code(ERRORS.USER_NOT_FOUND)
