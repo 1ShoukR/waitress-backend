01bbec541a14f5fc174a9144f60e3c025bb31b05
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-01-06 02:35:56 -0500

    created basic route for restaurants

--- webapp/api/routes/restaurant.py
+++ webapp/api/routes/restaurant.py
@@ -9,12 +9,21 @@ from flask import (
 )
 from passlib.hash import sha256_crypt
 from ... import models
+from ...utils.general import validate_incoming
+from ...utils.auth import authcheck
+from ...utils import auth
 bp = Blueprint('restaurant', __name__)
 
 
 
 @bp.route('/create', methods=['POST'])
+@authcheck(auth.authgroups.admin.super)
+@validate_incoming(required=['name'])
 def create():
+    # Owner ID will be attached to a user created in db
+    data = {k: v for k, v in request.form.items()}
+    new_restaurant = models.Restaurant(**data)
+    print(new_restaurant)
     return "I am working"
 
 
