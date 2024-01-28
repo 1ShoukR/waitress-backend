41264b04361d956affca2fd025ed433af67af922
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-01-27 19:47:25 -0500

    added basic barebones route for creating restauanrats future use

--- webapp/api/routes/restaurant.py
+++ webapp/api/routes/restaurant.py
@@ -18,12 +18,21 @@ bp = Blueprint('restaurant', __name__)
 
 @bp.route('/create', methods=['POST'])
 @authcheck(auth.authgroups.admin.super)
-@validate_incoming(required=['name'])
+@validate_incoming(required=['name', 'address', 'phone', 'email', 'website', 'owner_id'])
 def create():
     # Owner ID will be attached to a user created in db
-    data = {k: v for k, v in request.form.items()}
-    new_restaurant = models.Restaurant(**data)
-    print(new_restaurant)
+    owner = models.User.query.filter_by(user_id=request.form.get('owner_id'), access_revoked=None).first_or_404()
+    print('owner',owner)
+    # data = {k: v for k, v in request.form.items()}
+    new_restaurant = models.Restaurant(
+        name = request.form.get('name'), 
+        address = request.form.get('address'),
+        phone = request.form.get('phone'),
+        email = request.form.get('email'),
+        website = request.form.get('website'),
+        owner_id = owner
+    )
+    print(new_restaurant.serialize())
     return "I am working"
 
 
