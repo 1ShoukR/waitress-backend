1418f9cd80f5b4c700f95b1f820d225a23dc7800
Rahmin <rahminshoukoohi@gmail.com>
2024-04-16 23:08:40 -0400

    adjusted the server struct to accomodate separate routing

--- internal/server/server.go
+++ internal/server/server.go
@@ -8,31 +8,35 @@ import (
 	"time"
 
 	_ "github.com/joho/godotenv/autoload"
-  
+	"github.com/gin-gonic/gin"
+
 	"waitress-backend/internal/database"
-  
+	"waitress-backend/internal/server/routes"
 )
 
 type Server struct {
-	port int
-  
-	db   database.Service
-  
+	port   int
+	db     database.Service
+	router *gin.Engine // Add the Gin Engine to the Server struct
 }
 
 func NewServer() *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	NewServer := &Server{
-		port: port,
-  
-		db:   database.New(),
-  
+	newServer := &Server{
+		port:   port,
+		db:     database.New(),
+		router: gin.Default(), // Initialize the Gin Engine here
 	}
 
-	// Declare Server config
+	// Setup route groups
+	routes.UserRoutes(newServer.router)
+	// routes.ProductRoutes(newServer.router)
+	// ... include other route groups as needed
+
+	// Configure the HTTP server
 	server := &http.Server{
-		Addr:         fmt.Sprintf(":%d", NewServer.port),
-		Handler:      NewServer.RegisterRoutes(),
+		Addr:         fmt.Sprintf(":%d", newServer.port),
+		Handler:      newServer.router, // Use the Gin router as the handler
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
