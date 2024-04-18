d065c9363f5ceeb35954d8e69a21d2a9a3e99ea8
Rahmin <rahminshoukoohi@gmail.com>
2024-04-18 02:33:48 -0400

    passed gorm db instance into UserRoutes function and then pass to handler

--- internal/server/routes/user.go
+++ internal/server/routes/user.go
@@ -3,12 +3,15 @@ package routes
 import (
     "github.com/gin-gonic/gin"
     "waitress-backend/internal/handlers"
+    "gorm.io/gorm"
 )
 
-func UserRoutes(router *gin.Engine) {
+
+func UserRoutes(router *gin.Engine, db *gorm.DB) {
     user := router.Group("/users")
     {
         user.GET("/create", handlers.CreateUser)
+        user.POST("/get", handlers.GetUser(db))
         // user.POST("/", handlers.CreateUser)
         // user.GET("/:id", handlers.GetUser)
         // user.PUT("/:id", handlers.UpdateUser)
