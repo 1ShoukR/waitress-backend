f6b87e08bb96ee56ebcf95d22d9f80b65d387881
Rahmin <rahminshoukoohi@gmail.com>
2024-04-18 02:34:20 -0400

    created GetUser db function that returns all users

--- internal/handlers/userHandlers.go
+++ internal/handlers/userHandlers.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"waitress-backend/internal/models"
 )
 
 
@@ -11,5 +13,16 @@ func CreateUser(c *gin.Context) {
     response := "Test User Created"
     c.IndentedJSON(http.StatusOK, response)
     // logic to create a new user
+}
+
+func GetUser(db *gorm.DB) gin.HandlerFunc {
+    return func(c *gin.Context) {
+        var users []models.User
+        results := db.Find(&users)
+        if results.Error != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"Message": results.Error.Error()})
+        }
+        c.IndentedJSON(http.StatusOK, users)
+    }
 }
 
