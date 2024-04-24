c16e9026309a88f6514d0c68b175ff839ab1b22e
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-04-24 13:45:29 -0400

    grouped restauranty routes together in handlers

---
+++ internal/server/routes/restaurant.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"waitress-backend/internal/handlers"
+	"gorm.io/gorm"
+)
+
+
+func RestaurantRoutes (router *gin.Engine, db *gorm.DB) {
+	restaurantRoutes := router.Group("api/restaurant")
+	{
+		restaurantRoutes.POST("/create", handlers.CreateRestaurant(db))
+	}
+} 
