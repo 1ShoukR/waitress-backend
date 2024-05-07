9063cff29ff6fc871672e0af15694fca96fc6ada
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-05-07 02:32:33 -0400

    created CheckPassword and HashPassword funcs

--- internal/utilities/auth.go
+++ internal/utilities/auth.go
@@ -3,36 +3,17 @@ package utilities
 import (
 	// "log"
 	// "fmt"
-
-	"crypto/rand"
-	"crypto/sha256"
-	"encoding/base64"
+    "golang.org/x/crypto/bcrypt"
 )
 
 
 
-func GenerateSalt(size int) ([]byte, error) {
-    salt := make([]byte, size)
-    _, err := rand.Read(salt)
-    if err != nil {
-        return nil, err
-    }
-    return salt, nil
+func CheckPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
 }
 
-func HashPassword(password string, salt []byte) string {
-	sha256Hasher := sha256.New()
-	sha256Hasher.Write(salt)
-	sha256Hasher.Write([]byte(password))
-	hashedPassword := sha256Hasher.Sum(nil)
-	return base64.StdEncoding.EncodeToString(hashedPassword) 
-}
-
-func VerifyPassword(storedHash, password string, salt []byte) bool {
-    // Hash the provided password with the same salt
-    sha256Hasher := sha256.New()
-    sha256Hasher.Write(salt)
-    sha256Hasher.Write([]byte(password))
-    hashedPassword := sha256Hasher.Sum(nil)
-    return base64.StdEncoding.EncodeToString(hashedPassword) == storedHash
+func HashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
 }
