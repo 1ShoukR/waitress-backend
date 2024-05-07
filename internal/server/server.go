0004b9fa645cadb1e40b4e4ade55a38478cdbeea
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-05-07 17:36:38 -0400

    removed seeding functionality from NewServer

--- internal/server/server.go
+++ internal/server/server.go
@@ -7,14 +7,12 @@ import (
 	"os"
 	"strconv"
 	"time"
-	"waitress-backend/internal/models"
 	"waitress-backend/internal/server/routes"
 
 	gormsessions "github.com/gin-contrib/sessions/gorm"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
-	"waitress-backend/internal/database"
 
 	"github.com/gin-contrib/sessions"
 	// "github.com/gin-contrib/sessions/cookie"
@@ -28,40 +26,6 @@ type Server struct {
 	router *gin.Engine // Add the Gin Engine to the Server struct
 }
 
-func migrateDB(db *gorm.DB) error {
-    // Migrate the User table first because it's likely referenced by other tables
-    if err := db.AutoMigrate(&models.Entity{}); err != nil {
-        return fmt.Errorf("failed to migrate Entity table: %w", err)
-    }
-    if err := db.AutoMigrate(&models.User{}); err != nil {
-        return fmt.Errorf("failed to migrate User table: %w", err)
-    }
-
-    // Migrate the APIClient table if no dependencies on other custom tables
-    if err := db.AutoMigrate(&models.APIClient{}); err != nil {
-        return fmt.Errorf("failed to migrate APIClient table: %w", err)
-    }
-
-    // Migrate the Restaurant table next; it may depend on User
-    if err := db.AutoMigrate(&models.Restaurant{}); err != nil {
-        return fmt.Errorf("failed to migrate Restaurant table: %w", err)
-    }
-	// Migrate the Reservation Table
-    if err := db.AutoMigrate(&models.Reservation{}); err != nil {
-        return fmt.Errorf("failed to migrate Reservation table: %w", err)
-    }
-
-    // Migrate the Receipt table as it depends on Restaurant (and possibly User)
-    if err := db.AutoMigrate(&models.Receipt{}); err != nil {
-        return fmt.Errorf("failed to migrate Receipt table: %w", err)
-    }
-	if err:= database.Seeder.Seed(&database.UserSeeder{}, db); err != nil {
-		return err
-	}
-	println("seeded")
-
-    return nil
-}
 
 func NewServer() *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
@@ -87,7 +51,7 @@ func NewServer() *http.Server {
 	routes.UserRoutes(newServer.router, db)
 	routes.AuthRoutes(newServer.router, db)
 	routes.RestaurantRoutes(newServer.router, db)
-	// routes.ProductRoutes(newServer.router)
+	routes.UtilitiesRoutes(newServer.router, db)
 	// ... include other route groups as needed
 	
 	// Configure the HTTP server
@@ -97,10 +61,6 @@ func NewServer() *http.Server {
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
-	}
-	// db.AutoMigrate(&models.User{}, &models.APIClient{}, &models.Restaurant{}, &models.Receipt{})
-	if err := migrateDB(db); err != nil {
-	log.Fatalf("failed to migrate database tables: %v", err)
 	}
 	
 	return server
