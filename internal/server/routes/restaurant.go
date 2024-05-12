7e061d5dada501c95e6e6adbaec0ad8d5fea2853
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-05-11 23:57:48 -0400

    initialized GetLocalRestuarants handler

--- internal/server/routes/restaurant.go
+++ internal/server/routes/restaurant.go
@@ -12,6 +12,7 @@ func RestaurantRoutes(router *gin.Engine, db *gorm.DB) {
 	{
 		restaurantRoutes.POST("/create", handlers.CreateRestaurant(db, router))
 		restaurantRoutes.POST("/edit", handlers.EditRestaurant(db, router))
+		restaurantRoutes.POST("/local", handlers.GetLocalRestaurants(db, router))
 		restaurantRoutes.POST("/reservations/:restaurantId/get", handlers.GetReservations(db, router))
 	}
 } 
