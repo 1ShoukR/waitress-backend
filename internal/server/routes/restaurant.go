4a627ce4c6f51b6de5a22f8ace118c318f7b4dd9
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-05-13 01:32:24 -0400

    added authenitcation routes to restaurant routes

--- internal/server/routes/restaurant.go
+++ internal/server/routes/restaurant.go
@@ -1,18 +1,26 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
 	"waitress-backend/internal/handlers"
+	"waitress-backend/internal/utilities"
+
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 
 func RestaurantRoutes(router *gin.Engine, db *gorm.DB) {
-	restaurantRoutes := router.Group("api/restaurant")
-	{
-		restaurantRoutes.POST("/create", handlers.CreateRestaurant(db, router))
-		restaurantRoutes.POST("/edit", handlers.EditRestaurant(db, router))
-		restaurantRoutes.POST("/local", handlers.GetLocalRestaurants(db, router))
-		restaurantRoutes.POST("/reservations/:restaurantId/get", handlers.GetReservations(db, router))
-	}
-} 
+    userGroups := utilities.NewUserGroups() // Initialize your user groups
+    authGroups := utilities.NewAuthGroups(userGroups) // Create the auth groups from user groups
+
+    restaurantRoutes := router.Group("api/restaurant")
+    {
+        // Use AuthGroups to apply middleware
+        restaurantRoutes.POST("/create", utilities.UserRequired(authGroups, "Admin", "all"), handlers.CreateRestaurant(db, router))
+        restaurantRoutes.POST("/edit", utilities.UserRequired(authGroups, "Admin", "all"), handlers.EditRestaurant(db, router))
+        
+        // Applying more specific or different groups as needed
+        restaurantRoutes.POST("/local", utilities.UserRequired(authGroups, "Customer", "all"), handlers.GetLocalRestaurants(db, router))
+        restaurantRoutes.POST("/reservations/:restaurantId/get", utilities.UserRequired(authGroups, "Staff", "all"), handlers.GetReservations(db, router))
+    }
+}
