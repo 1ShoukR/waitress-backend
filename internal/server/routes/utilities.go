3a27131c5b32c2eee86eba95fdaa733de910c1ef
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-05-13 01:32:56 -0400

    added new security to database routes

--- internal/server/routes/utilities.go
+++ internal/server/routes/utilities.go
@@ -4,13 +4,16 @@ import (
     "github.com/gin-gonic/gin"
     "waitress-backend/internal/handlers"
     "gorm.io/gorm"
+	"waitress-backend/internal/utilities"
 )
 
 
 func UtilitiesRoutes(router *gin.Engine, db *gorm.DB) {
+	userGroups := utilities.NewUserGroups() // Initialize your user groups
+	authGroups := utilities.NewAuthGroups(userGroups) // Create the auth groups from user groups
 	database := router.Group("api/db")
 	{
-		database.GET("/seed", handlers.Seed(db))
-		database.GET("/run-all", handlers.RunAll(db))
+		database.GET("/seed", utilities.UserRequired(authGroups, "Dev", "all"), handlers.Seed(db))
+		database.GET("/run-all", utilities.UserRequired(authGroups, "Dev", "all"), handlers.RunAll(db))
 	}
 }
