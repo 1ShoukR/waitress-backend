45be550da357bb7992d61e9e8fba087378eda2ba
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-06-30 23:49:39 -0400

    created payments model file to hold payment models'

---
+++ internal/models/payments.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"time"
+)
+
+type Payment struct {
+	PaymentID         uint       `gorm:"primaryKey;autoIncrement:true"`
+	UserID            uint       `gorm:"not null"`
+	RestaurantID      uint       `gorm:"not null"`
+	StripePaymentKey  string     `gorm:"size:255;not null"`
+	Amount            float64    `gorm:"not null"`
+	Currency          string     `gorm:"size:3;not null"` // ISO 4217 currency code
+	Status            string     `gorm:"size:50;not null"` // e.g., 'pending', 'completed', 'failed'
+	PaymentMethod     string     `gorm:"size:50;not null"` // e.g., 'card', 'bank_transfer'
+	Description       string     `gorm:"size:255"`
+	CreatedAt         time.Time
+	FinalizedAt       *time.Time
+	// Relationships
+	// User              User      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+	Restaurant        Restaurant `gorm:"foreignKey:RestaurantID;"`
+}
