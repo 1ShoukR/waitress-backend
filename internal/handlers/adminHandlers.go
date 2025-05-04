038790a2dc0605e86ad79b2724ddfc22d95f7655
1ShoukR <rahminshoukoohi@gmail.com>
2025-05-04 02:42:27 -0400

    added restaurants to preloaded data

--- internal/handlers/adminHandlers.go
+++ internal/handlers/adminHandlers.go
@@ -15,13 +15,21 @@ func GetAdminData(db *gorm.DB, router *gin.Engine) gin.HandlerFunc {
 		
 		returnData := make(map[string]interface{})
 		var user models.User
+		var restaurants []models.Restaurant
 		
-		if err := db.First(&user, userId).Error; err != nil {
+		if err := db.Preload("Entity").First(&user, userId).Error; err != nil {
 			c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
 			return
 		}
 		
+		if err:= db.Find(&restaurants, user.UserID).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Restaurant not found"})
+			return
+		}
+		
 		returnData["user"] = user
+		returnData["entity"] = user.Entity
+		returnData["restaurant"] = restaurants
 		c.JSON(http.StatusOK, gin.H{"success": returnData})
 	}
 }
